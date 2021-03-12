a22984da95a8f6b6343bc09d9aabf04cc6c17574
underpressure <underpressure96@gmail.com>
2021-03-12 17:37:46 +0200

    lesson2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ export class AppComponent implements OnInit{
   title = 'hello world,im glad to see u all';
   count = 1;
   private lol = true;
+  dynamicImage = 'https://angular.kr/assets/images/logos/angular/angular.png';
+  angularImage = 'https://angular.kr/assets/images/logos/angular/angular.png'
+  inputText = ''
   ngOnInit() {
     this.title = 'Hello btch';
     let maza = 123;
@@ -28,4 +31,14 @@ export class AppComponent implements OnInit{
     }
     this.count--;
   }
+  constructor() {
+    setTimeout( () => {
+      console.log('Timeout is over')
+      this.dynamicImage = 'https://user-images.githubusercontent.com/5693916/30366646-10dc3b30-986c-11e7-871a-4f5c237b9a6f.png'
+    },5000)
+  };
+  onInput(event:any) {
+    this.inputText = event.target.value
+  }
 }
+
